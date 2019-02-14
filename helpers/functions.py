0cc280b494d2b8c0cdddbcfe150083d3f8c6e67b
CharlesPeterMcCarthy <charles@studybundles.com>
2019-02-14 10:58:48 +0000

    Moved Authentication To Imports

--- helpers/functions.py
+++ helpers/functions.py
@@ -2,6 +2,7 @@ import decimal
 import boto3
 import json
 import random
+import helpers.tables as tbl
 from passlib.context import CryptContext
 
 def DecimalDefault(obj):
@@ -41,3 +42,20 @@ def FormResponse(body):
 
 def GenerateAccessToken():
     return str("%032x" % random.getrandbits(128))
+
+def AuthUser(data):
+    try:
+        userTable = GetDataTable(tbl.AUTH)
+        res = userTable.get_item(Key={ 'StudentID': data['StudentID'] })
+    except:
+        res = { 'error': 'Database error' }
+
+    res = res['Item'] if 'Item' in res else { 'error': 'No user matches that Student ID - Force Logout' }   # Error codes to be setup
+
+    if 'AuthToken' in res:
+        if res['AuthToken'] != data['AuthToken']:
+            res = { 'error': 'Auth Token is invalid - Force Logout' }    # Error codes to be setup
+    if 'error' not in res:
+        res = { 'AuthOk': True }
+
+    return FormResponse(res)
--- private_api/users/change_timetable.py
+++ private_api/users/change_timetable.py
@@ -10,14 +10,13 @@ def ChangeTimetable(data):
     try:
         userTable = fnc.GetDataTable(tbl.USERS)
         res = userTable.get_item(Key={'StudentID': data['StudentID']})
-        res = res['Item'] if 'Item' in res else { 'error': 'No user matches that Student ID' }
 
         res = userTable.update_item(
             Key={'StudentID': data['StudentID']},
             UpdateExpression="set TimetableURL = :t",
             ExpressionAttributeValues={':t': data['TimetableURL']},
             ReturnValues="UPDATED_NEW"
-        )
+        ) if 'Item' in res else { 'error': 'No user matches that Student ID' }
     except:
         res = { 'error': 'Unknown error' }
 
