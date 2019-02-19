5384c580e930c59a49b4ee758f0ab5c5c50976f4
CharlesPeterMcCarthy <charles@studybundles.com>
2019-02-19 23:31:04 +0000

    Login Errors Improved

--- private_api/users/login.py
+++ private_api/users/login.py
@@ -1,25 +1,28 @@
 import json
 import helpers.functions as fnc
 import helpers.tables as tbl
+import helpers.errors as err
 
 def Handler(event, context):
     data = json.loads(event['body'])
-    return CheckLoginDetails(data) if 'studentID' in data and 'password' in data else fnc.FormResponse({ 'error': 'Missing Details' })
+    return CheckLoginDetails(data) if 'studentID' in data and 'password' in data else fnc.ErrorResponse(err.MISSING_DETAILS)
 
 def CheckLoginDetails(data):
     try:
         userTable = fnc.GetDataTable(tbl.USERS)
-        res = userTable.get_item(Key={'StudentID': data['studentID']})
-        res = res['Item'] if 'Item' in res else { 'error': 'No user matches that Student ID' }
-
-        if 'Password' in res:
-            if fnc.VerifyPassword(data['password'], res['Password']):
-                del res['Password']
-            else:
-                res = { 'error': 'Password is incorrect' }
-        if 'error' not in res:
-            res = { 'user': res, 'access-token': fnc.GenerateAccessToken() }
+        res = userTable.get_item(Key={ 'StudentID': data['studentID'] })
     except:
-        res = { 'error': 'Unknown error' }
+        return fnc.ErrorResponse(err.UNKNOWN)
 
-    return fnc.FormResponse(res)
+    if 'Item' in res:
+        res = res['Item']
+    else:
+        return fnc.ErrorResponse(err.INVALID_STUDENTID)
+
+    if 'Password' in res:
+        if fnc.VerifyPassword(data['password'], res['Password']):
+            del res['Password']
+        else:
+            return fnc.ErrorResponse(err.WRONG_PASSWORD)
+
+    return fnc.SuccessResponse({ 'user': res, 'Access-Token': fnc.GenerateAccessToken() })
