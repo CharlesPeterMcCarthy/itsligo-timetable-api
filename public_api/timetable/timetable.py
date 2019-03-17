dd0e64274b2bb79cc8a4cee0d2ae76c18edcdcd8
CharlesPeterMcCarthy <charles@studybundles.com>
2019-03-17 01:19:53 +0000

    Flag Conflicting Class Times

--- public_api/timetable/timetable.py
+++ public_api/timetable/timetable.py
@@ -15,9 +15,25 @@ def Handler(event, context):
     if not data['includeClasses']:
         timetable['days'] = RemoveClasses(timetable['days'])
 
+    if 'checkConflicts' in data and data['checkConflicts']:
+        timetable['days'] = CheckConflicts(timetable['days'])
+
     return fnc.SuccessResponse({ 'timetable': timetable })
 
 def RemoveClasses(days):
     for day in days:
         del day['classes']
+    return days
+
+def CheckConflicts(days):
+    for day in days:
+        classes = day['classes']
+        if not classes: continue
+        for i in range(len(classes)):
+            curClassTimes = classes[i]['times']
+            nextClassTimes = classes[i + 1]['times'] if i + 1 < len(classes) else None
+            if nextClassTimes:
+                if curClassTimes['start'] == nextClassTimes['start'] or curClassTimes['end'] == nextClassTimes['end'] or (curClassTimes['start'] < nextClassTimes['start'] and curClassTimes['end'] > nextClassTimes['start']):
+                    classes[i]['conflicting'] = True
+                    classes[i + 1]['conflicting'] = True
     return days
