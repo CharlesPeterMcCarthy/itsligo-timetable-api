16254aef9665946058c1a8e4eac1d1d8af765724
CharlesPeterMcCarthy <charles@studybundles.com>
2019-03-23 22:37:12 +0000

    StudentID Uppercase Login - Bug Fix

--- private_api/users/login.py
+++ private_api/users/login.py
@@ -8,9 +8,11 @@ def Handler(event, context):
     return CheckLoginDetails(data) if fnc.ContainsAllData(data, ('studentID', 'password')) else fnc.ErrorResponse(err.MISSING_DETAILS)
 
 def CheckLoginDetails(data):
+    studentID = data['studentID'].upper()
+
     try:
         userTable = fnc.GetDataTable(tbl.USERS)
-        res = userTable.get_item(Key={ 'studentID': data['studentID'] })
+        res = userTable.get_item(Key={ 'studentID': studentID })
     except: return fnc.ErrorResponse(err.DB_QU)
 
     if 'Item' in res: res = res['Item']
@@ -22,6 +24,6 @@ def CheckLoginDetails(data):
     else: return fnc.ErrorResponse(err.WRONG_PASSWORD)
 
     authToken = fnc.GenerateAuthToken()
-    authRes = fnc.UpdateAuthToken(data['studentID'], authToken)
+    authRes = fnc.UpdateAuthToken(studentID, authToken)
 
     return fnc.SuccessResponse({ 'user': res, 'authToken': authToken }) if 'updated' in authRes and authRes['updated'] else fnc.ErrorResponse(authRes)
